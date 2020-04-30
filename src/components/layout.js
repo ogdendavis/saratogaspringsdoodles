0b7f7a84ae1563165fa8e10b9d1b01bdcf9116ce
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-04-30 10:58:17 -0400

    Allow full-width layout on homepage

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,6 +31,7 @@ const Layout = ({ children, location }) => {
       }
     }
   `);
+  console.log(location);
 
   return (
     <>
@@ -38,8 +39,8 @@ const Layout = ({ children, location }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          maxWidth: location.pathname === '/' ? '100vw' : '960px',
+          padding: location.pathname === '/' ? '0' : `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
