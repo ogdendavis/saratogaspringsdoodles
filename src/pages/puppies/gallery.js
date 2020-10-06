3a5e7762664339ef546e2be408919714acd25c1e
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-10-06 16:59:37 -0400

    Add photo gallery

--- src/pages/puppies/gallery.js
+++ src/pages/puppies/gallery.js
@@ -1,8 +1,32 @@
+/*
+ * A couple of possible ways to make this interactive: click to pull up modal
+ * image (scrollable?), load image rows as they scroll into view
+ */
+
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import styled from 'styled-components';
 
 import Layout from '../../components/layout';
 import SEO from '../../components/seo';
+
+const GalleryContainer = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+`;
+
+const GalleryImage = styled.img`
+  width: 370px;
+  height: 370px;
+  border: 1px solid ${props => props.theme.offWhite};
+  object-fit: cover;
+
+  @media only screen and (max-width: 1160px) {
+    width: 50%;
+    height: auto;
+  }
+`;
 
 const GalleryPage = ({ location }) => {
   const data = useStaticQuery(graphql`
@@ -24,19 +48,21 @@ const GalleryPage = ({ location }) => {
   `);
 
   const allImages = data.allMarkdownRemark.edges.map(({ node }) => (
-    <img
+    <GalleryImage
       src={node.frontmatter.image}
       alt={node.frontmatter.title}
       key={`gallery-${node.id}`}
     />
   ));
 
+  console.log(allImages);
+
   return (
     <Layout location={location}>
       <SEO title="Puppy photo gallery" />
       <h1>Photo Gallery</h1>
       <p>This will be a photo gallery of puppies</p>
-      {allImages}
+      <GalleryContainer>{allImages}</GalleryContainer>
     </Layout>
   );
 };
