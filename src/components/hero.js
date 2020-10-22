84db0faffcef8f780277fa9ffa7434934387b44a
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-10-22 12:19:18 -0400

    Fix bad video filename

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import herovid from '../images/SarasotaSpringsDoodles.mp4';
+import herovid from '../images/SaratogaSpringsDoodles.mp4';
 
 const HeroContainer = styled.div``;
 
