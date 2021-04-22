0fd0ffc2b915d859a1930bbf067259a0a7207466
Lucas Ogden-Davis <lucas@ogdendavis.com>
2021-04-22 10:59:03 -0400

    Add placeholder litter page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,84 @@
+const path = require('path');
+
 /**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
+ * Programmatically create pages from litters in CMS
  */
+exports.createPages = ({ graphql, actions }) => {
+  // Extract createPage function (hook?) from actions object
+  const { createPage } = actions;
+  // Grab template that will be used to render pages
+  const litterPageTemplate = path.resolve('src/components/litterPage.js');
+
+  // Query to grab info needed for litter pages from graphql
+  return graphql(`
+    query litterPagesQuery {
+      litters: allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/cms/litters/.*.md/" } }
+        sort: { order: ASC, fields: [frontmatter___date] }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              date(formatString: "MMMM YYYY")
+              reservation_list
+              colors
+              size {
+                min
+                max
+              }
+              dam {
+                dam_name
+                dam_in_house
+                dam_image
+              }
+              sire {
+                sire_name
+                sire_in_house
+                sire_image
+              }
+              dub_sire {
+                dub_sire_name
+                dub_sire_in_house
+                dub_sire_image
+              }
+            }
+            html
+          }
+        }
+      }
+    }
+  `).then(result => {
+    // Once we have data from graphql, do things!
+    // First check for errors
+    if (result.errors) {
+      throw result.errors;
+    }
+
+    // Grab litters
+    const litters = result.data.litters.edges;
+
+    // If we only want to make pages for some litters, this is where we would filter them!
+
+    // TODO: for litters with same title, generate unique slug
+    // Use random number or ID? Must keep consistent for reproduction on front end
+
+    // Now create the pages!
+    litters.forEach(litter => {
+      // Generate slug from title
+      const slug = litter.node.frontmatter.title
+        .toLowerCase()
+        .split(' ')
+        .join('-');
+      console.log(slug);
+      // Publish the page using the template
+      createPage({
+        path: `litters/${slug}`,
+        component: litterPageTemplate,
+        context: {
+          content: litter.node,
+        },
+      });
+    });
+  });
+};
---
+++ src/components/litterPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Layout from './layout';
+import SEO from './seo';
+
+const LitterPage = ({ location, pageContext }) => {
+  // Content passed from gatsby-node.js lives in pageContext.content
+  const { content } = pageContext;
+  console.log(content);
+  return (
+    <Layout location={location}>
+      <SEO title={content.frontmatter.title} />
+      <h1>{content.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: content.html }} />
+    </Layout>
+  );
+};
+
+export default LitterPage;
