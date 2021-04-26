459891338435af9f18e400f8966142ede7701575
Lucas Ogden-Davis <lucas@ogdendavis.com>
2021-04-26 10:17:00 -0400

    Update litterCard content & styling

--- src/components/litterCard.js
+++ src/components/litterCard.js
@@ -4,59 +4,30 @@ import styled from 'styled-components';
 
 import logo from '../images/saratogaspringsdoodles.webp';
 
-const LitterContainer = styled.section`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  align-items: stretch;
-
-  margin: 0 auto 2rem;
-  box-shadow: 0px 2px 10px rgba(0, 64, 64, 0.15);
-  border-radius: 5px;
-  box-sizing: border-box;
+const LitterContainer = styled(Link)`
   background: #fff;
-
-  h2 {
-    width: 100%;
-  }
-
-  a {
-    text-decoration: none;
-    color: teal;
-    transition: all 0.3s ease-in-out;
-    position: relative;
-  }
-  a:hover {
-    color: orange;
-  }
-  a::before {
-    content: '';
-    position: absolute;
-    bottom: -3px;
-    width: 0px;
-    height: 3px;
-    margin: 3px 0 0;
-    transition: all 0.4s ease-in-out;
-    opacity: 0;
-    background-color: orange;
-  }
-  a:hover::before {
-    width: 100%;
-    opacity: 1;
-    left: 0;
-  }
-`;
-
-const ParentContainer = styled.div`
-  width: 35%;
-  min-width: 280px;
+  box-shadow: 0px 2px 10px rgba(0, 64, 64, 0.15);
+  box-sizing: border-box;
+  border-radius: 5px;
+  color: hsla(0, 0%, 0%, 0.8);
   display: flex;
-  flex-flow: column nowrap;
+  flex-flow: row nowrap;
+  margin: 0 auto 2rem;
   overflow: hidden;
+  text-decoration: none;
+  transition: box-shadow 0.5s ease;
 
-  @media only screen and (max-width: 855px) {
-    width: 100%;
-    flex-flow: row nowrap;
+  &:hover {
+    box-shadow: 0px 2px 15px orange;
+
+    span {
+      background: orange;
+      color: white;
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    flex-flow: row wrap;
     justify-content: center;
   }
 `;
@@ -65,55 +36,20 @@ const ParentImg = styled.img`
   object-fit: cover;
   object-position: 50% 20%;
   overflow: hidden;
+  width: 12rem;
 
-  /* Max Height */
-  ${props =>
-    props.sireCount === 2
-      ? 'max-height: 25vh; &:first-child {max-height: 45vh;}'
-      : props.sireCount === 1
-      ? 'max-height: 45vh;'
-      : 'min-height: 100%;'}
-
-  /* Border Radius */
-  ${props =>
-    props.sireCount === 0
-      ? 'border-radius: 5px 0 0 5px'
-      : props.sireCount === 1
-      ? 'border-radius: 5px 0 0 0; &:last-child {border-radius: 0 0 0 5px;}'
-      : '&:first-child {border-radius: 5px 0 0 0;} &:last-child {border-radius: 0 0 0 5px;}'};
-
-  @media only screen and (max-width: 855px) {
-    width: ${props =>
-      props.sireCount === 2
-        ? '33.3333%'
-        : props.sireCount === 1
-        ? '50%'
-        : '100%'};
-    height: 100%;
-    max-height: 50vh;
-
-    /* Border Radius */
-    ${props =>
-      props.sireCount === 0
-        ? 'border-radius: 5px 5px 0 0'
-        : props.sireCount === 1
-        ? 'border-radius: 5px 0 0 0; &:last-child {border-radius: 0 5px 0 0;}'
-        : '&:first-child {border-radius: 5px 0 0 0;} &:last-child {border-radius: 0 5px 0 0;}'};
+  @media only screen and (max-width: 768px) {
+    width: 50%;
   }
 `;
 
 const LitterInfo = styled.div`
-  box-size: border-box;
-  margin: auto;
+  flex-grow: 1;
   padding: 1rem;
-  width: 65%;
 
-  p:last-child {
-    margin-bottom: 0;
-  }
-
-  @media only screen and (max-width: 855px) {
-    width: 95%;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    text-align: center;
   }
 `;
 
@@ -126,28 +62,33 @@ const LitterFrontmatter = styled.ul`
   }
 `;
 
-const ReservationList = styled.div`
-  h3 {
-    margin: 1rem 0 0.5rem;
-  }
-
-  ol {
-    margin: 0 0 1em 1em;
-  }
-
-  li {
-    margin: 0;
-  }
-`;
-
-const CallToAction = styled.p`
-  margin-top: 0.5rem;
+const CardButton = styled.span`
+  background: #fff;
+  border: 3px solid orange;
+  border-radius: 5px;
+  color: orange;
+  display: inline-block;
+  font-weight: 700;
+  font-size: 1.2em;
+  margin-top: 1rem;
+  padding: 0.5em 1em;
+  transition: all 0.5s ease;
 `;
 
 const LitterCard = ({ litter, dogImagePaths }) => {
+  // Create slug for link to litter page, same as in gatsby-node
+  const slug =
+    '/litters/' +
+    `${litter.frontmatter.title} ${litter.frontmatter.date}`
+      .toLowerCase() // convert to all lowercase
+      .replace(/[^A-Za-z0-9 ]/g, '') // strip non-word characters
+      .replace('invalid date', 'coming soon') // remove invalid date from tbd litters
+      .split(' ') // create array of words
+      .join('-'); // join words with dashes
+
   // Check for presence of sire and dual sire -- affects text & styling
   const hasSire = !(
-    litter.frontmatter.sire['sire_image'] === null &&
+    !litter.frontmatter.sire['sire_image'] &&
     litter.frontmatter.sire['sire_name'] === 'TBD'
   );
   const isDual =
@@ -155,43 +96,6 @@ const LitterCard = ({ litter, dogImagePaths }) => {
       ? true
       : false;
   const sireCount = isDual && hasSire ? 2 : hasSire ? 1 : 0;
-
-  // Get link to sire / dam profile, if in-house
-  // First construct the ids used in dogCard
-  const sireId = litter.frontmatter.sire.sire_name.includes(' ')
-    ? litter.frontmatter.sire.sire_name.split(' ')[0]
-    : litter.frontmatter.sire.sire_name;
-  const damId = litter.frontmatter.dam.dam_name.includes(' ')
-    ? litter.frontmatter.dam.dam_name.split(' ')[0]
-    : litter.frontmatter.dam.dam_name;
-  const dubSireId = !isDual
-    ? null
-    : litter.frontmatter.dub_sire.dub_sire_name.includes(' ')
-    ? litter.frontmatter.dub_sire.dub_sire_name.split(' ')[0]
-    : litter.frontmatter.dub_sire.dub_sire_name;
-
-  const sire = litter.frontmatter.sire.sire_in_house ? (
-    <Link to={`/meet-the-dogs#${sireId}`}>
-      {litter.frontmatter.sire.sire_name}
-    </Link>
-  ) : (
-    litter.frontmatter.sire.sire_name
-  );
-  const dam = litter.frontmatter.dam.dam_in_house ? (
-    <Link to={`/meet-the-dogs#${damId}`}>
-      {litter.frontmatter.dam.dam_name}
-    </Link>
-  ) : (
-    litter.frontmatter.dam.dam_name
-  );
-  const dubSire = !isDual ? null : litter.frontmatter.dub_sire
-      .dub_sire_in_house ? (
-    <Link to={`/meet-the-dogs#${dubSireId}`}>
-      {litter.frontmatter.dub_sire.dub_sire_name}
-    </Link>
-  ) : (
-    litter.frontmatter.dub_sire.dub_sire_name
-  );
 
   // Get path to sire / dam image: attached to litter, attached to in-house dog, or default img
   const damImage = litter.frontmatter.dam.dam_image ? (
@@ -241,31 +145,23 @@ const LitterCard = ({ litter, dogImagePaths }) => {
     <ParentImg sireCount={sireCount} src={logo} alt="dog face" />
   );
 
-  // Format reservations
-  const reservations = litter.frontmatter['reservation_list']
-    ? litter.frontmatter['reservation_list'].map((item, index) => (
-        <li key={`${dam}-${sire}-${index}`}>{item}</li>
-      ))
-    : [];
-
   // Title text changes if dual sire litter
   const title = isDual ? (
     <h2>
-      {dam} with {sire} and {dubSire}
+      {litter.frontmatter.dam.dam_name} with {litter.frontmatter.sire.sire_name}{' '}
+      and {litter.frontmatter.dub_sire.dub_sire_name}
     </h2>
   ) : (
     <h2>
-      {dam} and {sire}
+      {litter.frontmatter.dam.dam_name} and {litter.frontmatter.sire.sire_name}
     </h2>
   );
 
   return (
-    <LitterContainer>
-      <ParentContainer>
-        {damImage}
-        {sireImage}
-        {isDual && dubSireImage}
-      </ParentContainer>
+    <LitterContainer to={slug}>
+      {damImage}
+      {sireImage}
+      {isDual && dubSireImage}
       <LitterInfo>
         {title}
         <LitterFrontmatter>
@@ -293,17 +189,7 @@ const LitterCard = ({ litter, dogImagePaths }) => {
             </li>
           )}
         </LitterFrontmatter>
-        <div dangerouslySetInnerHTML={{ __html: litter.html }} />
-        {litter.frontmatter['reservation_list'] && (
-          <ReservationList>
-            <h3>Reservation List</h3>
-            <ol>{reservations}</ol>
-          </ReservationList>
-        )}
-        <CallToAction>
-          Interested in a puppy from this litter? Fill out our{' '}
-          <Link to="/puppies/application">application</Link>.
-        </CallToAction>
+        <CardButton>Learn More</CardButton>
       </LitterInfo>
     </LitterContainer>
   );
